08e03d3252053ee56048014c9c09383b1d2ac146
rikutech <rikutech@gmail.com>
2018-08-07 01:34:22 +0900

    impl form error handling

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -5,7 +5,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts.User
 
   def index(conn, _params) do
-    users = Accounts.list_users()
+    users = Accounts.list_users
     render(conn, "index.html", users: users)
   end
 
@@ -20,9 +20,13 @@ defmodule RumblWeb.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    {:ok, user} = Accounts.create_user(user_params)
-    conn
-    |> put_flash(:info, "#{user.name} created!")
-    |> redirect(to: user_path(conn, :index))
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
